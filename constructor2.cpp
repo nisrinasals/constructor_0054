31bc6d3e02bfd6b202991ade1a2ab49101420ec7
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-13 16:01:54 +0700

    menuliskan int main dan selesai

--- constructor2.cpp
+++ constructor2.cpp
@@ -38,3 +38,17 @@ Mahasiswa::Mahasiswa(int iNim, string iNama)
         nama = iNama;
     };
 
+int main()
+{
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Ronaldo");
+    Mahasiswa mhs4(30, "Messi");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
