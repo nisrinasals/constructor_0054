a295bf974e85cbcc403bcc7a5a04a74044358ec5
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-13 14:42:33 +0700

    class mahasiswa selesai

--- constructor.cpp
+++ constructor.cpp
@@ -6,4 +6,12 @@ public:
     static int nim;
     int id;
     string nama;
-}
+
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama):nama(pnama)
+    {
+        setID();
+    }
+};
