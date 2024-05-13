d5f0a2e701e10d073249e0a6476cabf773e64cbd
nisrinasals <nisrinahikmawan@gmail.com>
2024-05-13 15:29:48 +0700

    menuliskan modifier public pada kelas Mahasiswa

--- constructor2.cpp
+++ constructor2.cpp
@@ -2,3 +2,21 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+
+    }
+};
